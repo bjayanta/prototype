800f9185518d10592dc5568e9ff5f931463f2efe
Jayanta Biswas <bjayanta.me@gmail.com>
2019-03-06 13:48:13 +0600

    work with alert message

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        {{-- include meta and title --}}
         @include('admin.layouts.head')
     </head>
 
     <body>
         <div id="app">
+            {{-- include header --}}
             @include('admin.layouts.header')
 
-            @section('main-content')
-                @show
+            <main>
+                <div class="container">
+                    {{-- include confirmation message --}}
+                    @include('admin.layouts.partials.alert')
+
+                    {{-- include main content --}}
+                    @section('content')
+                        @show
+                </div>
+            </main>
         </div>
 
+        {{-- include footer --}}
         @include('admin.layouts.footer')
     </body>
 </html>
